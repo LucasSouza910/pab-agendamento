db52762068f85933cf6fcd5255712e0e88279b12
LucasSouza910 <89272961+LucasSouza910@users.noreply.github.com>
2021-10-22 16:34:14 -0300

    Add files via upload

---
+++ www/js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCDT04xH-NO60XYjl4wUfZ6wkPSiHTiju4",
+    authDomain: "pab-1-40819.firebaseapp.com",
+    projectId: "pab-1-40819",
+    storageBucket: "pab-1-40819.appspot.com",
+    messagingSenderId: "726863334306",
+    appId: "1:726863334306:web:6b405c806fbea39cd400fb",
+    measurementId: "G-VGWYQEVX4L"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
